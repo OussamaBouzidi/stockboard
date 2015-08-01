5575d3c6acb1292492c5a8d3032660a67de0fb5e
skl92 <stanleykliu92@gmail.com>
2015-08-01 16:22:03 -0700

    redo submit form

--- public/js/app.js
+++ public/js/app.js
@@ -28628,8 +28628,19 @@ var minlengthDirective = function() {
 
 (function() {
   angular.module('stockboard.controllers.nav', [])
-  .controller('NavCtrl', function() {
+  .controller('NavCtrl', function($scope) {
     console.log('This nav should be on every page.');
+    $scope.loggedIn = true;
+    $scope.recordStockWatch = function() {
+      console.log('recorded watch');
+      $scope.recordWatch = true;
+      $scope.recordPurchase = false;
+    }
+    $scope.recordStockPurchase = function() {
+      console.log('recorded purchase');
+      $scope.recordPurchase = true;
+      $scope.recordWatch = false;
+    }
   });
 })();
 
--- src/js/controllers/nav.js
+++ src/js/controllers/nav.js
@@ -1,6 +1,17 @@
 (function() {
   angular.module('stockboard.controllers.nav', [])
-  .controller('NavCtrl', function() {
+  .controller('NavCtrl', function($scope) {
     console.log('This nav should be on every page.');
+    $scope.loggedIn = true;
+    $scope.recordStockWatch = function() {
+      console.log('recorded watch');
+      $scope.recordWatch = true;
+      $scope.recordPurchase = false;
+    }
+    $scope.recordStockPurchase = function() {
+      console.log('recorded purchase');
+      $scope.recordPurchase = true;
+      $scope.recordWatch = false;
+    }
   });
 })();
