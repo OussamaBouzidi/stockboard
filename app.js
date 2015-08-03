4be30a3b5fe676fc2a889be27a2d9c8fb3ce906c
skl92 <stanleykliu92@gmail.com>
2015-08-03 13:24:54 -0700

    change package json

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('src', path.join(__dirname, 'src'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
