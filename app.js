6a1c31096a57863e687649a0e504fa8308c06c7d
skl92 <stanleykliu92@gmail.com>
2015-08-11 00:11:57 -0700

    chnage callback url

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  callbackURL: 'http://stockboard-stanley.herokuapp.com/auth/google/callback',
   passReqToCallback: true},
   function(req, accessToken, refreshToken, profile, done){
     User.findOne({ email: profile.emails[0].value }, function(error, user) {
